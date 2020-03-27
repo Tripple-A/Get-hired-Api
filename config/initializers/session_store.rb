ddbb17cec0dccd91eadfbb88958406f9025dce55
Biodun <biodun9@gmail.com>
2020-03-27 10:15:39 +0100

    added key corresponding to app name to cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,8 +2,8 @@
 
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store,
-                                         key: '_gethired_app',
-                                         domain: :all
+                                         key: '_foodie-apiv1',
+                                         domain: 'https://foodie-apiv1.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store,
                                          key: '_gethired_app'
