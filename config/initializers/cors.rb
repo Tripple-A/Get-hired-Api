444f17a0fcd46e11197aadc83c7d0f7bfda6e16c
Adebola <onedebos@gmail.com>
2020-05-18 22:19:37 +0100

    Update cors.rb
    App "netlify.app" to fix CORS error.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,4 +18,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              methods: %i[get post put patch delete options head],
              credentials: true
   end
+
+  allow do
+    origins 'https://foodies-stop.netlify.app'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
+  end
 end
